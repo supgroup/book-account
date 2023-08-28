8c84acb4db64ee5c579ed6b44549083d61209292
ahmadms2020 <70610331+ahmadms2020@users.noreply.github.com>
2023-08-28 12:27:41 +0300

    Update FileClass.cs

--- BookAccountApp/BookAccountApp/Classes/ApiClasses/FileClass.cs
+++ BookAccountApp/BookAccountApp/Classes/ApiClasses/FileClass.cs
@@ -26,232 +26,209 @@ namespace BookAccountApp.ApiClasses
         public string notes { get; set; }
         public Nullable<bool> isActive { get; set; }
         public Nullable<int> tableRowId { get; set; }
-
+        public bool canDelete { get; set; }
         /// <summary>
         /// ///////////////////////////////////////
         /// </summary>
         /// <returns></returns>
         /// 
-        //public async Task<List<FileClass>> GetAll()
-        //{
+        public async Task<List<FileClass>> GetAll()
+        {
 
-        //    List<Flights> List = new List<Flights>();
-        //    bool canDelete = false;
-        //    try
-        //    {
-        //        using (bookdbEntities entity = new bookdbEntities())
-        //        {
-        //            List = (from S in entity.flights
-        //                    select new Flights()
-        //                    {
-        //                        flightId = S.flightId,
-        //                        airlineflightTable = S.airline + "/" + S.flightTable.name,
-        //                        airline = S.airline,
-        //                        flight = S.flightTable.name == null ? "" : S.flightTable.name,
-        //                        flightFrom = S.fromTable.name == null ? "" : S.fromTable.name,
-        //                        flightTo = S.toTable.name,
-        //                        notes = S.notes,
-        //                        createDate = S.createDate,
-        //                        updateDate = S.updateDate,
-        //                        createUserId = S.createUserId,
-        //                        updateUserId = S.updateUserId,
-        //                        flightTableId = S.flightTableId == null ? 0 : S.flightTableId,
-        //                        fromTableId = S.fromTableId == null ? 0 : S.fromTableId,
-        //                        toTableId = S.toTableId == null ? 0 : S.toTableId,
+            List<FileClass> List = new List<FileClass>();
+            bool canDelete = false;
+            try
+            {
+                using (bookdbEntities entity = new bookdbEntities())
+                {
+                    List = (from S in entity.passengerFiles
+                            select new FileClass()
+                            {
+                                fileId = S.fileId,
+                                fileName = S.fileName,
+                                extention = S.extention,
+                                folderName = S.folderName,
+                                notes = S.notes,
+                                isActive = S.isActive,
+                                tableRowId = S.passengerId,
 
-        //                        canDelete = true,
+                                canDelete = false,
 
-        //                    }).ToList();
+                            }).ToList();
 
-        //            //if (List.Count > 0)
-        //            //{
-        //            //    for (int i = 0; i < List.Count; i++)
-        //            //    {
-        //            //        if (List[i].isActive == 1)
-        //            //        {
-        //            //            int userId = (int)List[i].userId;
-        //            //            var itemsI = entity.packageUser.Where(x => x.userId == userId).Select(b => new { b.userId }).FirstOrDefault();
+                    //if (List.Count > 0)
+                    //{
+                    //    for (int i = 0; i < List.Count; i++)
+                    //    {
+                    //        if (List[i].isActive == true)
+                    //        {
+                    //            int itemId = (int)List[i].fileId;
+                    //            var itemsI = entity.serviceData.Where(x => x.fileId == itemId).Select(b => new { b.fileId }).FirstOrDefault();
 
-        //            //            if ((itemsI is null))
-        //            //                canDelete = true;
-        //            //        }
-        //            //        List[i].canDelete = canDelete;
-        //            //    }
-        //            //}
-        //            return List;
-        //        }
+                    //            if (itemsI is null)
+                    //            {
+                    //                List[i].canDelete = true;
+                    //            }
+                    //        }
 
-        //    }
-        //    catch
-        //    {
-        //        return List;
-        //    }
-        //}
+                    //    }
+                    //}
+                    return List;
+                }
 
-        //public async Task<decimal> Save(Flights newitem)
-        //{
-        //    flights newObject = new flights();
-        //    newObject = JsonConvert.DeserializeObject<flights>(JsonConvert.SerializeObject(newitem));
+            }
+            catch
+            {
+                return List;
+            }
+        }
 
-        //    decimal message = 0;
-        //    if (newObject != null)
-        //    {
-        //        if (newObject.updateUserId == 0 || newObject.updateUserId == null)
-        //        {
-        //            Nullable<int> id = null;
-        //            newObject.updateUserId = id;
-        //        }
-        //        if (newObject.createUserId == 0 || newObject.createUserId == null)
-        //        {
-        //            Nullable<int> id = null;
-        //            newObject.createUserId = id;
-        //        }
+        public async Task<decimal> Save(FileClass newitem)
+        {
+            passengerFiles newObject = new passengerFiles();
+            newObject = JsonConvert.DeserializeObject<passengerFiles>(JsonConvert.SerializeObject(newitem));
+
+            decimal message = 0;
+            if (newObject != null)
+            {
+                if (newObject.passengerId == 0 || newObject.passengerId == null)
+                {
+                    Nullable<int> id = null;
+                    newObject.passengerId = id;
+                }
+              
 
 
 
-        //        try
-        //        {
-        //            using (bookdbEntities entity = new bookdbEntities())
-        //            {
-        //                var locationEntity = entity.Set<flights>();
-        //                if (newObject.flightId == 0)
-        //                {
-        //                    newObject.createDate = DateTime.Now;
-        //                    newObject.updateDate = newObject.createDate;
-        //                    newObject.updateUserId = newObject.createUserId;
+                try
+                {
+                    using (bookdbEntities entity = new bookdbEntities())
+                    {
+                        var locationEntity = entity.Set<passengerFiles>();
+                        if (newObject.fileId == 0)
+                        {
+                         
+
+                            newObject.isActive = true;
+                            locationEntity.Add(newObject);
+                            entity.SaveChanges();
+                            message = newObject.fileId;
+                        }
+                        else
+                        {
+                            var tmpObject = entity.passengerFiles.Where(p => p.fileId == newObject.fileId).FirstOrDefault();
+
+                     
+
+                            tmpObject.fileId = newObject.fileId;
+                            tmpObject.fileName = newObject.fileName;
+                            tmpObject.extention = newObject.extention;
+                            tmpObject.folderName = newObject.folderName;
+                            tmpObject.notes = newObject.notes;
+                            tmpObject.isActive = newObject.isActive;
+                            tmpObject.passengerId = newObject.passengerId;
+
+                            entity.SaveChanges();
+
+                            message = tmpObject.fileId;
+                        }
+                    }
+                    return message;
+                }
+                catch
+                {
+                    return 0;
+                }
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        public async Task<FileClass> GetByID(int itemId)
+        {
 
 
-        //                    locationEntity.Add(newObject);
-        //                    entity.SaveChanges();
-        //                    message = newObject.flightId;
-        //                }
-        //                else
-        //                {
-        //                    var tmpObject = entity.flights.Where(p => p.flightId == newObject.flightId).FirstOrDefault();
-
-        //                    tmpObject.updateDate = DateTime.Now;
-
-        //                //    tmpObject.flightId = newObject.flightId;
-        //                    tmpObject.airline = newObject.airline;
-        //                    tmpObject.flightTableId = newObject.flightTableId;
-        //                    tmpObject.fromTableId = newObject.fromTableId;
-        //                    tmpObject.toTableId = newObject.toTableId;
-        //                    tmpObject.notes = newObject.notes;
-        //                    tmpObject.createDate = newObject.createDate;
-
-        //                    tmpObject.createUserId = newObject.createUserId;
-        //                    tmpObject.updateUserId = newObject.updateUserId;
+            FileClass item = new FileClass();
 
 
-        //                    entity.SaveChanges();
+            FileClass row = new FileClass();
+            try
+            {
+                using (bookdbEntities entity = new bookdbEntities())
+                {
+                    var list = entity.passengerFiles.ToList();
+                    row = list.Where(u => u.fileId == itemId)
+                     .Select(S => new FileClass()
+                     {
+                         fileId = S.fileId,
+                         fileName = S.fileName,
+                         extention = S.extention,
+                         folderName = S.folderName,
+                         notes = S.notes,
+                         isActive = S.isActive,
+                         tableRowId = S.passengerId,
 
-        //                    message = tmpObject.flightId;
-        //                }
-        //            }
-        //            return message;
-        //        }
-        //        catch
-        //        {
-        //            return 0;
-        //        }
-        //    }
-        //    else
-        //    {
-        //        return 0;
-        //    }
-        //}
-        //public async Task<Flights> GetByID(int itemId)
-        //{
+                     }).FirstOrDefault();
+                    return row;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                row = new FileClass();
+                //userrow.name = ex.ToString();
+                return row;
+            }
+        }
+        public async Task<decimal> Delete(int id, int signuserId, bool final)
+        {
+
+            decimal message = 0;
+            if (final)
+            {
+                try
+                {
+                    using (bookdbEntities entity = new bookdbEntities())
+                    {
+                        passengerFiles objectDelete = entity.passengerFiles.Find(id);
+
+                        entity.passengerFiles.Remove(objectDelete);
+                        message = entity.SaveChanges();
+                        return message;
+
+                    }
+                }
+                catch
+                {
+                    return 0;
+
+                }
+            }
+            else
+            {
+                try
+                {
+                    using (bookdbEntities entity = new bookdbEntities())
+                    {
+                        passengerFiles objectDelete = entity.passengerFiles.Find(id);
+
+                        objectDelete.isActive = false;
+                      
+                        message = entity.SaveChanges();
+
+                        return message;
+                    }
+                }
+                catch
+                {
+                    return 0;
+                }
+            }
 
 
-        //    Flights item = new Flights();
+        }
 
-
-        //    Flights row = new Flights();
-        //    try
-        //    {
-        //        using (bookdbEntities entity = new bookdbEntities())
-        //        {
-        //            var list = entity.flights.ToList();
-        //            row = list.Where(u => u.flightId == itemId)
-        //             .Select(S => new Flights()
-        //             {
-        //                 flightId = S.flightId,
-        //                 airline = S.airline,
-        //                 flight = S.flightTable.name == null ? "" : S.flightTable.name,
-        //                 flightFrom = S.fromTable.name == null ? "" : S.fromTable.name,
-        //                 flightTo = S.toTable.name,
-        //                 notes = S.notes,
-        //                 createDate = S.createDate,
-        //                 updateDate = S.updateDate,
-        //                 createUserId = S.createUserId,
-        //                 updateUserId = S.updateUserId,
-        //                 flightTableId = S.flightTableId == null ? 0 : S.flightTableId,
-        //                 fromTableId = S.fromTableId == null ? 0 : S.fromTableId,
-        //                 toTableId = S.toTableId == null ? 0 : S.toTableId,
-
-
-
-        //             }).FirstOrDefault();
-        //            return row;
-        //        }
-
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        row = new Flights();
-        //        //userrow.name = ex.ToString();
-        //        return row;
-        //    }
-        //}
-        //public async Task<decimal> Delete(int id, int signuserId, bool final)
-        //{
-
-        //    decimal message = 0;
-        //    if (final)
-        //    {
-        //        try
-        //        {
-        //            using (bookdbEntities entity = new bookdbEntities())
-        //            {
-        //                flights objectDelete = entity.flights.Find(id);
-
-        //                entity.flights.Remove(objectDelete);
-        //                message = entity.SaveChanges();
-        //                return message;
-
-        //            }
-        //        }
-        //        catch
-        //        {
-        //            return 0;
-
-        //        }
-        //    }
-        //    return message;
-        //    //else
-        //    //{
-        //    //    try
-        //    //    {
-        //    //        using (bookdbEntities entity = new bookdbEntities())
-        //    //        {
-        //    //            flights objectDelete = entity.flights.Find(userId);
-
-        //    //            objectDelete.isActive = 0;
-        //    //            objectDelete.updateUserId = signuserId;
-        //    //        objectDelete.updateDate = DateTime.Now;
-        //    //            message = entity.SaveChanges() ;
-
-        //    //            return message;
-        //    //        }
-        //    //    }
-        //    //    catch
-        //    //    {
-        //    //        return 0;
-        //    //    }
-        //    //}
-
-        //}
 
 
     }
