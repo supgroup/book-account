78611d6fbb9124371a2679a5d94f81c0e8b7a751
yasinidlbi <yasinidlbi@gmail.com>
2023-08-30 02:52:22 +0300

    uc_users

---
+++ BookAccountApp/BookAccountApp/View/settings/users/uc_users.xaml.cs
@@ -0,0 +1,146 @@
+﻿using BookAccountApp.Classes;
+using BookAccountApp.View.windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BookAccountApp.View.settings.users
+{
+    /// <summary>
+    /// Interaction logic for uc_users.xaml
+    /// </summary>
+     public partial class uc_users : UserControl
+    {
+        public uc_users()
+        {
+            InitializeComponent();
+        }
+        private static uc_users _instance;
+        public static uc_users Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new uc_users();
+                return _instance;
+            }
+        }
+
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+            // Collect all generations of memory.
+            GC.Collect();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {//load
+            try
+            {
+                if (sender != null)
+                    HelpClass.StartAwait(grid_main);
+                //MainWindow.mainWindow.initializationMainTrack(this.Tag.ToString(), 1);
+
+                #region translate
+                if (MainWindow.lang.Equals("en"))
+                {
+                    //MainWindow.resourcemanager = new ResourceManager("POS.en_file", Assembly.GetExecutingAssembly());
+                    grid_main.FlowDirection = FlowDirection.LeftToRight;
+                }
+                else
+                {
+                    //MainWindow.resourcemanager = new ResourceManager("POS.ar_file", Assembly.GetExecutingAssembly());
+                    grid_main.FlowDirection = FlowDirection.RightToLeft;
+                }
+
+                translate();
+                #endregion
+
+                if (sender != null)
+                    HelpClass.EndAwait(grid_main);
+            }
+            catch (Exception ex)
+            {
+                if (sender != null)
+                    HelpClass.EndAwait(grid_main);
+                HelpClass.ExceptionMessage(ex, this);
+            }
+        }
+        private void translate()
+        {
+            //txt_comInfo.Text = MainWindow.resourcemanager.GetString("trComInfo");
+            //txt_comHint.Text = MainWindow.resourcemanager.GetString("trSettingHint");
+        }
+
+
+        private void Btn_addUser_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                HelpClass.StartAwait(grid_main);
+                /*
+                Window.GetWindow(this).Opacity = 0.2;
+                wd_addUser w = new wd_addUser();
+                w.ShowDialog();
+                Window.GetWindow(this).Opacity = 1;
+                */
+                HelpClass.EndAwait(grid_main);
+            }
+            catch (Exception ex)
+            {
+                HelpClass.EndAwait(grid_main);
+                HelpClass.ExceptionMessage(ex, this);
+            }
+        }
+
+        private void Btn_passwordChange_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                HelpClass.StartAwait(grid_main);
+                /*
+                Window.GetWindow(this).Opacity = 0.2;
+                wd_passwordChange w = new wd_passwordChange();
+                w.ShowDialog();
+                Window.GetWindow(this).Opacity = 1;
+                */
+                HelpClass.EndAwait(grid_main);
+            }
+            catch (Exception ex)
+            {
+                HelpClass.EndAwait(grid_main);
+                HelpClass.ExceptionMessage(ex, this);
+            }
+        }
+         private void Btn_permission_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                HelpClass.StartAwait(grid_main);
+                /*
+                Window.GetWindow(this).Opacity = 0.2;
+                wd_permission w = new wd_permission();
+                w.ShowDialog();
+                Window.GetWindow(this).Opacity = 1;
+                */
+                HelpClass.EndAwait(grid_main);
+            }
+            catch (Exception ex)
+            {
+                HelpClass.EndAwait(grid_main);
+                HelpClass.ExceptionMessage(ex, this);
+            }
+        }
+
+    }
+}
