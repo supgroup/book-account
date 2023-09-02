c514d681d70780b116e9a5aae1f619be7d22f59c
yasinidlbi <yasinidlbi@gmail.com>
2023-09-02 16:17:14 +0300

    wd_rateSyrSoto

---
+++ BookAccountApp/BookAccountApp/View/windows/wd_rateSyrSoto.xaml.cs
@@ -0,0 +1,299 @@
+﻿using BookAccountApp.ApiClasses;
+using BookAccountApp.Classes;
+using netoaster;
+using POS.View.windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BookAccountApp.View.windows
+{
+    /// <summary>
+    /// Interaction logic for wd_rateSyrSoto.xaml
+    /// </summary>
+    public partial class wd_rateSyrSoto : Window
+    {
+        #region variables
+
+       
+
+
+        public static List<string> requiredControlList;
+
+        BrushConverter bc = new BrushConverter();
+
+
+        #endregion
+
+        public wd_rateSyrSoto()
+        {
+            try
+            {
+                InitializeComponent();
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this);
+            }
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {//load
+            try
+            {
+                if (sender != null)
+                    HelpClass.StartAwait(grid_windowMain);
+                requiredControlList = new List<string> { "name" };
+                #region translate
+                //if (AppSettings.lang.Equals("en"))
+                //{
+                //    MainWindow.resourcemanager = new ResourceManager("BookAccountApp.en_file", Assembly.GetExecutingAssembly());
+                //    grid_main.FlowDirection = FlowDirection.LeftToRight;
+                //}
+                //else
+                //{
+                //    MainWindow.resourcemanager = new ResourceManager("BookAccountApp.ar_file", Assembly.GetExecutingAssembly());
+                grid_main.FlowDirection = FlowDirection.RightToLeft;
+                //}
+                translate();
+                #endregion
+
+
+
+
+                if (sender != null)
+                    HelpClass.EndAwait(grid_windowMain);
+            }
+            catch (Exception ex)
+            {
+                if (sender != null)
+                    HelpClass.EndAwait(grid_windowMain);
+                HelpClass.ExceptionMessage(ex, this);
+            }
+
+        }
+
+        #region methods
+        private void translate()
+        {
+            /*
+            txt_title.Text = MainWindow.resourcemanager.GetString("officeInfo");
+
+            MaterialDesignThemes.Wpf.HintAssist.SetHint(tb_name, MainWindow.resourcemanager.GetString("officeNameHint"));
+            MaterialDesignThemes.Wpf.HintAssist.SetHint(tb_mobile, MainWindow.resourcemanager.GetString("mobileNumHint"));
+
+
+            btn_add.Content = MainWindow.resourcemanager.GetString("trAdd");
+            btn_update.Content = MainWindow.resourcemanager.GetString("trUpdate");
+            btn_delete.Content = MainWindow.resourcemanager.GetString("trDelete");
+            */
+        }
+
+       
+       
+        public static bool validate(List<string> requiredControlList, UserControl userControl)
+        {
+            bool isValid = true;
+            try
+            {
+                foreach (var control in requiredControlList)
+                {
+                    TextBox textBox = FindControls.FindVisualChildren<TextBox>(userControl).Where(x => x.Name == "tb_" + control)
+                        .FirstOrDefault();
+                    Path path = FindControls.FindVisualChildren<Path>(userControl).Where(x => x.Name == "p_error_" + control)
+                        .FirstOrDefault();
+                    if (textBox != null && path != null)
+                        if (!HelpClass.validateEmpty(textBox.Text, path))
+                            isValid = false;
+                }
+                foreach (var control in requiredControlList)
+                {
+                    ComboBox comboBox = FindControls.FindVisualChildren<ComboBox>(userControl).Where(x => x.Name == "cb_" + control)
+                        .FirstOrDefault();
+                    Path path = FindControls.FindVisualChildren<Path>(userControl).Where(x => x.Name == "p_error_" + control)
+                        .FirstOrDefault();
+                    if (comboBox != null && path != null)
+                        if (!HelpClass.validateEmpty(comboBox.Text, path))
+                            isValid = false;
+                }
+                foreach (var control in requiredControlList)
+                {
+                    PasswordBox passwordBox = FindControls.FindVisualChildren<PasswordBox>(userControl).Where(x => x.Name == "pb_" + control)
+                        .FirstOrDefault();
+                    Path path = FindControls.FindVisualChildren<Path>(userControl).Where(x => x.Name == "p_error_" + control)
+                        .FirstOrDefault();
+                    if (passwordBox != null && path != null)
+                        if (!HelpClass.validateEmpty(passwordBox.Password, path))
+                            isValid = false;
+                }
+                #region Email
+                //  IsValidEmail(userControl);
+                #endregion
+
+
+            }
+            catch { }
+            return isValid;
+
+        }
+
+
+
+    
+
+        #endregion
+
+        #region events
+
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                DragMove();
+            }
+            catch //(Exception ex)
+            {
+                //HelpClass.ExceptionMessage(ex, this);
+            }
+        }
+        private void HandleKeyPress(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (e.Key == Key.Return)
+                {
+                    //Btn_save_Click(null, null);
+                }
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this);
+            }
+        }
+        private void Window_Unloaded(object sender, RoutedEventArgs e)
+        {
+            GC.Collect();
+        }
+        private void Btn_colse_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this);
+            }
+        }
+
+
+      
+        string input;
+        decimal _decimal = 0;
+        private void Number_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        { //only  digits
+            try
+            {
+                TextBox textBox = sender as TextBox;
+                HelpClass.InputJustNumber(ref textBox);
+                //if (textBox.Tag.ToString() == "int")
+                //{
+                //    Regex regex = new Regex("[^0-9]");
+                //    e.Handled = regex.IsMatch(e.Text);
+                //}
+                //else if (textBox.Tag.ToString() == "decimal")
+                //{
+                input = e.Text;
+                e.Handled = !decimal.TryParse(textBox.Text + input, out _decimal);
+                //}
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this);
+            }
+        }
+
+        private void space_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                TextBox textBox = sender as TextBox;
+                HelpClass.InputJustNumber(ref textBox);
+                e.Handled = e.Key == Key.Space;
+            }
+            catch (Exception ex)
+            {
+
+                HelpClass.ExceptionMessage(ex, this);
+            }
+        }
+        
+        #endregion
+
+        #region repots
+
+
+
+        #endregion
+
+        #region add - update - delete
+        private async void Btn_save_Click(object sender, RoutedEventArgs e)
+        {//add
+            try
+            {
+
+
+                HelpClass.StartAwait(grid_main);
+
+               
+
+                HelpClass.EndAwait(grid_main);
+            }
+            catch (Exception ex)
+            {
+                HelpClass.EndAwait(grid_main);
+                HelpClass.ExceptionMessage(ex, this);
+            }
+        }
+        
+        #endregion
+
+        private void validateEmpty_LostFocus(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                HelpClass.validate(requiredControlList, this);
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this);
+            }
+        }
+
+        private void ValidateEmpty_TextChange(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                HelpClass.validate(requiredControlList, this);
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this);
+            }
+        }
+
+
+    }
+}
