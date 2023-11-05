e75679f56659f3ee84b9972264d7bb8549c2150a
ahmadms2020 <70610331+ahmadms2020@users.noreply.github.com>
2023-11-05 21:46:27 +0200

    travel

--- BookAccountApp/BookAccountApp/Properties/AssemblyInfo.cs
+++ BookAccountApp/BookAccountApp/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MOWAISHE")]
+[assembly: AssemblyTitle("MOWAISHE Travel")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MOWAISHE")]
+[assembly: AssemblyProduct("MOWAISHE Travel")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
